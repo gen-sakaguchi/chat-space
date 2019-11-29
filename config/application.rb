1d9817fc08cb5fd3843078fa076e1ebea3625d88
gen-sakaguchi <magpulmasada422@gmail.com>
2019-11-29 10:14:56 +0900

    あ
    あ

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'devise'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-require 'devise'
 
 module ChatSpace
   class Application < Rails::Application
